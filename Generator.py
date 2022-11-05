82681813fab0d2e5acc8fe17d5a7db0e750d6062
Patik Pejic <patrik.pejic@hotmail.com>
2022-11-06 00:02:22 +0100

    added the list with the possible password characters

--- Generator.py
+++ Generator.py
@@ -1,1 +1,9 @@
-test = input("Please Enter Something: ")
+password_length = 0
+
+letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
+'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+number_list = [i for i in range(10)]
+
+special_character_list = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+',
+ '<', '>', '/', '?', 'q', '~']
